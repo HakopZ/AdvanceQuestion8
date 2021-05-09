06e724fb82ed28bb77ba8325cdb5bc6e50dea091
HakopZarikyan <hakop2006@gmail.com>
2021-05-09 12:27:10 -0700

    Add project files.

---
+++ AdvanceQuestion8/CircularEnum.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace AdvanceQuestion8
+{
+    public struct CircularEnum<T> where T : Enum
+    {
+        private T en;
+        static Dictionary<int, T> enums = new Dictionary<int, T>();
+        static int position = 0;
+        public static implicit operator T(CircularEnum<T> temp) => temp.en; 
+        public static implicit operator CircularEnum<T>(T temp)
+        {
+
+            
+            var enumVals = Enum.GetValues(typeof(T));
+            foreach(var vals in enumVals)
+            {
+                enums.Add((int)vals, (T)vals);
+            }
+            CircularEnum<T> t;
+            t.en = temp;
+            return t;
+        }
+        
+        static CircularEnum()
+        {
+        }
+        public static CircularEnum<T> operator +(CircularEnum<T> a, int x)
+        {
+            if (x < 0) return a - (Math.Abs(x));
+            position += x;
+            if (position >= enums.Count) position -= enums.Count;
+            a.en = enums[position];
+            return a;
+        }
+        public static CircularEnum<T> operator -(CircularEnum<T> a, int x) => a + -x;
+        public static CircularEnum<T> operator ++(CircularEnum<T> a) => a + 1;
+        public static CircularEnum<T> operator --(CircularEnum<T> a) => a + -1;
+
+    }
+}
---
+++ AdvanceQuestion8/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AdvanceQuestion8
+{
+    class Program
+    {
+        enum Test
+        {
+            One,
+            Two,
+            Three
+        }
+        static void Main(string[] args)
+        {
+
+            CircularEnum<Test> temp = Test.One;
+            temp+= -2;
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
